5d4b9ea302ea580b7a64d42803a33f662aa067ea
pikalovvova1303 <pikalovvova1303@gmail.com>
2024-09-05 01:45:09 +0300

    v 1.0

--- Quadratic equation Solvers/Source.cpp
+++ Quadratic equation Solvers/Source.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 
-int TakeDiscr(int b, int a, int c) {
+int TakeDiscr(int b, int a, int c) {					//Discriminant Formula
 
 	int Discr = (b * b) - 4 * a * c;
 
 	return Discr;
 }
+
+int TakeRootInOneRoot(int b, int a) {					//Solo Root root formula
+
+	int RootX = -(b / (2 * a));
+
+	return RootX;
+}
+
+//
+int takeFirstRootInDoubleRoot(int Discr, int a, int b) { // first root in double root
+
+	int FirstRoot = (-b - sqrt(Discr)) / (2 * a);
+
+	return FirstRoot;
+}
+
+int takeSecondRootInDoubleRoot(int Discr, int a, int b) { // second root in double root
+
+	int SecondRoot = (-b + sqrt(Discr)) / (2 * a);
+
+	return SecondRoot;
+}
+
+//
 
 int main() {
 
@@ -16,6 +41,13 @@ int main() {
 	int a = 0;
 	int c = 0;
 	int Discr;
+
+	int FirstRoot = 0;
+	int SecondRoot = 0;
+
+	bool NoRoots = false;
+	bool OneRoot = false;
+	bool TwoRoots = false;
 
 	cout << "Enter B\n";
 	cin >> b;
@@ -28,15 +60,42 @@ int main() {
 
 	Discr = TakeDiscr(b, a, c);
 	cout << Discr;
+	cout << endl;
 
 	if (Discr == 0) {
 		cout << "One root.\n";
+
+		OneRoot = true;
 	}
 	else if (Discr > 0) {
 		cout << "Two roots\n";
+
+		TwoRoots = true;
 	}
 	else if (Discr < 0) {
 		cout << "No roots.\n";
+
+		NoRoots = true;
+	}
+
+	if (NoRoots) {
+		exit(0);
+	}
+	else if (OneRoot) {
+
+		FirstRoot = TakeRootInOneRoot(b, a);
+
+		cout << "Root = " << FirstRoot << endl;
+
+	}
+	else if (TwoRoots) {
+
+		FirstRoot = takeFirstRootInDoubleRoot(Discr, a, b);
+		SecondRoot = takeSecondRootInDoubleRoot(Discr, a, b);
+
+		cout << "First root - " << FirstRoot << endl;
+		cout << "Second root - " << SecondRoot << endl;
+
 	}
 
 }
